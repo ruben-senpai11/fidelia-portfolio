ede88fa072b2b5600dccde7d3a3c1fddaaf1e3d9
ruben-senpai <albanhonfovou@gmail.com>
2024-03-19 03:38:24 +0100

    004

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,6 +16,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <link rel="shortcut icon" href="icon.png" type="image/x-icon" />
       <body className={inter.className}>{children}</body>
     </html>
   );
