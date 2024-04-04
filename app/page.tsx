9f80b6be648be6fd190908d5a6921a93cd541fb5
ruben-senpai <albanhonfovou@gmail.com>
2024-04-04 02:20:45 +0100

    improved stuffs

--- app/page.tsx
+++ app/page.tsx
@@ -40,13 +40,13 @@ export default function Page({ searchParams }: { searchParams: { [key: string]: 
       <Header />
       <main className="flex flex-col items-center justify-between ">
         <section id="hero" className="">
-          <div className="introduction hero-background px-[0%] py-[15%] flex flex-col justify-center items-center gap-8">
+          <div className="introduction hero-background px-[0%] py-[10%] flex flex-col justify-center items-center gap-12">
             <h1 className="text-[18px] -mt-5">WHITE DEVS AGENCY</h1>
             <h2 className="text-[5vw] leading-[120%] text-start">
               Nous scalons votre business  de +40% en 6 mois avec un site web et du marketing
             </h2>
           </div>
-          <div className="introduction px-[1%] py-[10px] pb-16 flex flex-col justify-center items-center gap-6  ">
+          <div className="introduction px-[1%] py-[0px] pb-16 flex flex-col justify-center items-center gap-6  ">
             <div className="artisans flex flex-row justify-between gap-2 ">
               <div className="left-side border-solidd flex justify-center w-[50%] items-center gap-6">
                 <h2 className=" text-right text-[3.5vw] leading-snug ">
