22b0546693082916dd58226ee3159602ed589270
ruben-senpai <albanhonfovou@gmail.com>
2024-04-16 22:53:39 +0100

    interest bugs cleared

--- app/components/interest.tsx
+++ app/components/interest.tsx
@@ -9,18 +9,15 @@ export default function Interest({interestsList}:Props) {
 
   const [clickedInterests, setClickedInterests] = useState([]);
 
-  const handleClick = (index: number, e: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
-    e.preventDefault(); // Prevents the default behavior of the click event
+  const handleClick = (index: string , e: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
+    e.preventDefault(); 
 
-    // Check if the item is already clicked
-    const isClicked = clickedInterests.includes(index);
+    const isClicked = clickedInterests.includes(index as never);
 
-    // If it's already clicked, remove it from the array
     if (isClicked) {
       setClickedInterests((prevState) => prevState.filter(item => item !== index));
     } else {
-      // If it's not clicked, add it to the array
-      setClickedInterests((prevState) => [...prevState, index]);
+      setClickedInterests((prevState) => [...prevState, index as never]);
     }
   };
 
@@ -28,7 +25,7 @@ export default function Interest({interestsList}:Props) {
     <>
       <div className="flex flex-wrap gap-4">
         {interestsList.map((item, index) => (
-          <div key={index}  onClick={(e) => handleClick(index, e)} className={ (clickedInterests.includes(index) ? "clientBudget " : "") + "rounded uppercase cursor-pointer "}>{item}</div>
+          <div key={index}  onClick={(e) => handleClick(item, e)} className={ (clickedInterests.includes(item as never) ? "clientBudget " : "") + "rounded uppercase cursor-pointer "}>{item}</div>
         ))}
       </div>
     </>
--- app/navigation/header.tsx
+++ app/navigation/header.tsx
@@ -111,11 +111,11 @@ export default function Header() {
           <div className="nav-links items-center justify-center relative ">
             <ul className={(width > 1000 && isScrolled ? "opacity-95 hover:opacity-90 bg-zinc-700 " : "opacity-100 bg-neutral-800 ") + "flex items-center justify-between gap-6 max-w-100 px-4 py-[6px] text-[15px] font-medium transition-all "}>
               <li onClick={() => smoothScrollTo('home')} className={ (activeLink === 'hero' ? 'active ' : '') + (isScrolled ? "hover:text-slate-500  " : "hover:text-slate-800 ") + "uppercase hover:bg-slate-50 "}>
-                {/* <a href="#home" className="desktop"> */}
+                <a href="#home" className="desktop">
                   <svg width="16" height="15" viewBox="0 0 16 15" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
                     <path d="M9.13641 1.01599C8.8731 0.81116 8.54901 0.699951 8.21541 0.699951C7.88181 0.699951 7.55772 0.81116 7.29441 1.01599L1.00641 5.90599C0.44241 6.34624 0.75291 7.24999 1.46766 7.24999H2.21541V13.25C2.21541 13.6478 2.37345 14.0293 2.65475 14.3107C2.93605 14.592 3.31759 14.75 3.71541 14.75H6.71541V10.25C6.71541 9.85217 6.87345 9.47064 7.15475 9.18933C7.43605 8.90803 7.81759 8.74999 8.21541 8.74999C8.61323 8.74999 8.99477 8.90803 9.27607 9.18933C9.55737 9.47064 9.71541 9.85217 9.71541 10.25V14.75H12.7154C13.1132 14.75 13.4948 14.592 13.7761 14.3107C14.0574 14.0293 14.2154 13.6478 14.2154 13.25V7.24999H14.9632C15.6772 7.24999 15.9892 6.34624 15.4244 5.90674L9.13641 1.01599Z" fill="currentColor"></path>
                   </svg>
-                {/* </a>   */}
+                </a>  
                 <a href="#hero" className="mobile" >Accueil</a>
               </li>
               <li onClick={() => smoothScrollTo('services')} className={ (activeLink === 'services' ? 'active ' : '') + (isScrolled ? "hover:text-slate-700 " : "hover:text-slate-800 ") + "uppercase hover:bg-slate-50 "}>
